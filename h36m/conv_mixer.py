35c8c60ac90e9e62c9b4bd20851ef69f30ef4bb3
Valentin von Bornhaupt <s6vavonb@uni-bonn.de>
2023-08-23 19:20:24 +0200

    Minor Changes

--- h36m/conv_mixer.py
+++ h36m/conv_mixer.py
@@ -51,11 +51,13 @@ class MultiChanSELayer(nn.Module):
                     Output pose sequence of shape [batch_size, conv_nChan, in_nTP, dimPosEmb]
         """
         bs, _, in_nTP, _ = x.shape # [bs, conv_nChan, in_nTP, dimPosEmb]
-        # Squeeze:
+        
+        ##### Squeeze #####
         y = x.transpose(1, 2) # [bs, in_nTP, conv_nChan, dimPosEmb]
         y = self.squeezeBlock(y) # [bs, in_nTP, 1, 1]
         y = y.view(bs, in_nTP) # [bs, in_nTP]
-        # Excitation:
+        
+        ##### Excitation #####
         y = self.excitationBlock(y) # [bs, in_nTP]
         y = y.view(bs, in_nTP, 1, 1) # [bs, in_nTP, 1, 1]
         y = y.transpose(1, 2) # [bs, 1, in_nTP, 1]
